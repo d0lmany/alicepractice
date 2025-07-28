6a7bb4d49e75a79932ca5344d313ef7aeae4c9a3
d0lmany <d0lmany.is.god@gmail.com>
2025-07-28 21:02:09 +0500

    watch, adding stopwatch

--- watch/script.js
+++ watch/script.js
@@ -1,15 +1,67 @@
+// vars
 const hourPlace = document.getElementById('hours'), minutePlace = document.getElementById('minutes'),
 secondPlace = document.getElementById('seconds'), milSecPlace = document.getElementById('milliseconds');
+let intervals = {}, buffer = 0, cache = 0;
+// functions
 const now = () => {
     return new Date();
 }
-const updateBigWatch = () => {
-    hourPlace.textContent = now().getHours().toString().padStart(2, '0');
-    minutePlace.textContent = now().getMinutes().toString().padStart(2, '0');
-    secondPlace.textContent = now().getSeconds().toString().padStart(2, '0');
+const watchMode = () => {
+    resetIntervals();
+    intervals.updateBigWatch = setInterval(() => {
+        updateBigWatch(now);
+    }, 500);
+    intervals.updateMilSec = setInterval(() => {
+        updateMilSec(now);
+    }, 10);
 }
-const updateMilSec = () => {
-    milSecPlace.textContent = now().getMilliseconds().toString().padStart(3, '0');
+const stopwatchMode = () => {
+    resetIntervals();
+    resetUI();
+    intervals.sw = setInterval(() => {
+        buffer += 10;
+        cache = buffer;
+        const h = computeCache(3600 * 1000);
+        const m = computeCache(60 * 1000);
+        const s = computeCache(1000);
+        let date = new Date(2025, 1, 1, h, m, s, cache);
+        updateBigWatch(() => {return date});
+        updateMilSec(() => {return date});
+    }, 10);
 }
-setInterval(updateBigWatch, 500)
-setInterval(updateMilSec, 10)
+const resetIntervals = () => {
+    Object.values(intervals).forEach(i => {
+        clearInterval(i);
+    });
+    intervals = {};
+    buffer = 0;
+}
+const computeCache = (limit) => {
+    if (cache >= limit) {
+        const target = parseInt(cache / limit);
+        cache -= target * limit;
+        return target;
+    } else {
+        return 0;
+    }
+}
+// render
+const updateBigWatch = (call) => {
+    hourPlace.textContent = call().getHours().toString().padStart(2, '0');
+    minutePlace.textContent = call().getMinutes().toString().padStart(2, '0');
+    secondPlace.textContent = call().getSeconds().toString().padStart(2, '0');
+}
+const updateMilSec = (call) => {
+    milSecPlace.textContent = call().getMilliseconds().toString().padStart(3, '0');
+}
+const resetUI = () => {
+    hourPlace.textContent = '00';
+    minutePlace.textContent = '00';
+    secondPlace.textContent = '00';
+    milSecPlace.textContent = '000';
+}
+// listeners
+document.getElementById('watchMode').addEventListener('click', watchMode);
+document.getElementById('stopwatchMode').addEventListener('click', stopwatchMode);
+// main
+watchMode();
